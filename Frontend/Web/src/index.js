043ab4eb35830cc3c0bdf5d69e13467120bb5ca6
V-vey <htayag.1228@gmail.com>
2026-08-03 15:54:11 +0800

    ??

--- Frontend/Web/src/index.js
+++ Frontend/Web/src/index.js
@@ -1,14 +1,19 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+//login page
+// import LoginButton from "./component/Test Button/button";
+import LoginPage from "./component/Test Button/button";
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>
+    {/* <LoginButton /> */}
+  </React.StrictMode>,
 );
 
 // If you want to start measuring performance in your app, pass a function
---
+++ Frontend/Web/src/views/LoginPage/login_page.js
@@ -0,0 +1,12 @@
+function LoginPage() {
+  const logo = "Fuck you";
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Hello, World! {logo}</h1>
+      </header>
+    </div>
+  );
+}
+
+export default LoginPage;
