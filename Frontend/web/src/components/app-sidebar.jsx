b3cb2e1b3c4829f19421da32b3b6e9303c08fe68
V-vey <htayag.1228@gmail.com>
2026-09-03 07:48:31 +0800

    sidebar

--- Frontend/web/src/components/app-sidebar.jsx
+++ Frontend/web/src/components/app-sidebar.jsx
@@ -17,19 +17,24 @@ import {
   SidebarMenuButton,
   SidebarMenuItem,
   SidebarHeader,
+  SidebarFooter,
 } from "@/components/ui/sidebar";
+import { Button } from "@base-ui/react";
+
+import { Link, useNavigate } from "react-router-dom";
 
 // Your navigation links
 const items = [
-  { title: "Dashboard", url: "#", icon: LayoutDashboard },
-  { title: "Employee", url: "#", icon: User },
-  { title: "Attendance", url: "#", icon: Calendar },
-  { title: "Payroll", url: "#", icon: Search },
+  { title: "Dashboard", url: "/dashboard", icon: LayoutDashboard },
+  { title: "Employee", url: "/employee", icon: User },
+  { title: "Attendance", url: "/attendance", icon: Calendar },
+  { title: "Payroll", url: "/payroll", icon: Search },
 ];
 
 export function AppSidebar() {
+  const navigate = useNavigate();
   return (
-    <Sidebar>
+    <Sidebar className="w-65">
       <SidebarContent>
         <SidebarHeader>
           <div className="text-start p-2 font-bold text-xl">
@@ -42,15 +47,33 @@ export function AppSidebar() {
             {items.map((item) => (
               <SidebarMenuItem key={item.title}>
                 <SidebarMenuButton asChild>
-                  <a href={item.url}>
-                    <item.icon />
-                    <span>{item.title}</span>
-                  </a>
+                  <Link to={item.url}>
+                    <div className="flex items-center gap-2">
+                      <span className="shrink-0">
+                        <item.icon />
+                      </span>
+                      <span>{item.title}</span>
+                    </div>
+                  </Link>
                 </SidebarMenuButton>
               </SidebarMenuItem>
             ))}
           </SidebarGroup>
         </SidebarContent>
+        <SidebarFooter>
+          <Button
+            className=" w-full bg-[#2AAF56] 
+                      hover:bg-[#EC6668]
+                        rounded-[10px]
+                      text-white"
+            onClick={() => {
+              // localStorage.removeItem("token");
+              navigate("login");
+            }}
+          >
+            Log-out
+          </Button>
+        </SidebarFooter>
       </SidebarContent>
     </Sidebar>
   );
