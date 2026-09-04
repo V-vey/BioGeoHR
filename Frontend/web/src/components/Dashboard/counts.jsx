d840e4904d8aa993a16de2edd42da16dbb03183b
V-vey <htayag.1228@gmail.com>
2026-09-04 19:54:00 +0800

    cards update

--- Frontend/web/src/components/Dashboard/counts.jsx
+++ Frontend/web/src/components/Dashboard/counts.jsx
@@ -1,17 +1,28 @@
 //ICONS
 // import { Users } from "lucide-react";
 
-export default function countsLayout({ display, count, icon }) {
+export default function countsLayout({ display, count, icon, percentage }) {
   return (
     <>
-      <div class="flex-1 min-w-[150px] p-4 bg-white border border-gray-100 rounded-xl shadow-[0_0_6.3px_3px_rgba(0,0,0,0.25)]">
+      <div className="flex-1 min-w-37.5 px-3 py-2 bg-white border border-gray-100 rounded-xl shadow-[0_0_6.3px_3px_rgba(0,0,0,0.25)]">
         <div className="flex items-start font-bold text-[#6675EC]">
           {display}
         </div>
-        <div className="flex items-center gap-2">
-          <p className="text-[24px] font-bold text-[#3A3A3A]">{count}</p>
+        {/* <div className="h-0.5 w-full bg-[#E0E0E0] my-0.5" /> */}
 
-          <div style={{ flexGrow: 1 }} />
+        <div className="flex items-start justify-between">
+          {/* We use flex-col here to stack the items tightly */}
+          <div className="flex flex-col items-start leading-none">
+            <span className="text-[24px] font-bold text-[#3A3A3A]">
+              {count}
+            </span>
+            {percentage && (
+              <span className="text-[13px] font-normal text-[#2AAF56] mt-0.5">
+                {percentage}
+              </span>
+            )}
+          </div>
+
           <span>{icon}</span>
         </div>
       </div>
--- Frontend/web/src/outlet/DashboardOutlet.jsx
+++ Frontend/web/src/outlet/DashboardOutlet.jsx
@@ -1,27 +1,42 @@
 import Counts from "@/components/Dashboard/counts";
-
+import { useState, useEffect } from "react";
 // ICONS
 import { Users, Clock, History, TriangleAlert, DoorOpen } from "lucide-react";
 export default function DashboardOutlet() {
+  const [metrics, setMetrics] = useState({
+    countEmployees: 0,
+    countAttendance: 0,
+    countLate: 0,
+    countAbsent: 0,
+    countLeave: 0,
+
+    percentageAttendance: 0,
+    percentageLate: 0,
+    percentageAbsent: 0,
+    percentageLeave: 0,
+  });
+
   return (
     <>
       <div class="flex w-full justify-between gap-4">
         <Counts
           className="flex-1 "
           display="Total Employees"
-          count={"10,000"}
+          count={metrics.countEmployees}
           icon={<Users className="text-[#6675EC] w-11.5 h-11.5 text-[20px] " />}
         />
         <Counts
           className="flex-1"
           display="Total Attendance"
-          count={10}
+          count={metrics.countAttendance}
+          percentage={`${metrics.percentageAttendance}% of Employees`}
           icon={<Clock className="text-[#2AAF56] w-11.5 h-11.5 text-[20px] " />}
         />
         <Counts
           className="flex-1"
           display="Late Employees"
-          count={10}
+          count={metrics.countLate}
+          percentage={`${metrics.percentageLate}% of Employees`}
           icon={
             <History className="text-[#EACA3A] w-11.5 h-11.5 text-[20px] " />
           }
@@ -29,7 +44,8 @@ export default function DashboardOutlet() {
         <Counts
           className="flex-1"
           display="Absent Employees"
-          count={10}
+          count={metrics.countAbsent}
+          percentage={`${metrics.percentageAbsent}% of Employees`}
           icon={
             <TriangleAlert className="text-[#EC6668] w-11.5 h-11.5 text-[20px] " />
           }
@@ -37,7 +53,8 @@ export default function DashboardOutlet() {
         <Counts
           className="flex-1"
           display="Total Leave"
-          count={10}
+          count={metrics.countLeave}
+          percentage={`${metrics.percentageLeave}% of Employees`}
           icon={
             <DoorOpen className="text-[#6675EC] w-11.5 h-11.5 text-[20px] " />
           }
